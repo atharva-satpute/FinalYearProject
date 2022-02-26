6a4df55cd7ce84c93179626ce473985c2b191818
atharva-satpute <atharva_satpute@rediffmail.com>
2022-02-26 12:45:55 +0530

    Added Search Bar

--- client/src/DashBoard.jsx
+++ client/src/DashBoard.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import axios from 'axios';
+import { Box, Button, InputBase } from '@mui/material';
 import Navbar from "./components/Navbar/NavBar";
+
+import './DashBoard.css';
 
 
 class DashBoard extends React.PureComponent {
 
     state = {
-        isUserAuth: false
+        isUserAuth: false,
+        searchID: null,
+        selectedFile: null
     }
 
     isUserAuthCallback = (auth) => {
@@ -15,11 +20,63 @@ class DashBoard extends React.PureComponent {
         });
     }
 
+    onFileChange = (event) => {
+        this.setState({
+            selectedFile: (event) ? (event.target.files[0]) : null
+        });
+    }
+
     render() {
         return (
-            <>
+            <div className="dashboard-container">
                 <Navbar authCheck={this.state.isUserAuth}/>
-            </>
+                <div className="search-container">
+                    <div className="search-bar">
+                        <Box
+                            borderRadius={2}
+                            display='flex'
+                            sx={{ 
+                                flexGrow: 1,
+                                boxShadow: 2
+                            }}
+                        >
+                            <InputBase
+                                autoFocus
+                                fullWidth
+                                type="text"
+                                placeholder="Search Bug ID"
+                                sx={{ 
+                                    flexGrow: 1, 
+                                    borderRadius: 2,
+                                    paddingLeft: 2
+                                }}
+
+                                onChange={(event) => {
+                                    this.setState({
+                                        searchID: event.target.value
+                                    })
+                                }}
+                            />
+                        </Box>
+                        <Button
+                            variant="contained"
+                            sx={{
+                                marginLeft: 1
+                            }}
+                        >
+                            Search
+                        </Button>
+                    </div>
+                    <p>or</p>
+                    <input
+                        accept=".json, .csv"
+                        type={'file'}
+                        onChange={(event) => {
+                            this.onFileChange(event)
+                        }}
+                    />                 
+                </div>
+            </div>
         );
     }
 }
