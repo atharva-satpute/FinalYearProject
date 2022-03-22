fe8a447e9cf4fd08354de50dd066539cff9d6fb2
atharva-satpute <atharva_satpute@rediffmail.com>
2022-03-22 16:56:43 +0530

    Removed Routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import SignIn from './components/SignIn/SignIn';
-import SignUp from './components/SignUp/SignUp';
 import DashBoard from './DashBoard';
 
 import './index.css';
 
 
 ReactDOM.render(
-    <Router>
-        <Routes>
-            <Route path='/' exact element={<DashBoard />}/>
-            <Route path='/signIn' element={<SignIn />}/>
-            <Route path='/signUp' element={<SignUp />}/>
-        </Routes>
-    </Router>,
+    <DashBoard />,
     document.getElementById('root')
 );
